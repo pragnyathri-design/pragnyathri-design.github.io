7b0fb97060c8b7dcdff58333de2f03f27df4e6a2
pragnyathri-design <pragnyathri@gmail.com>
2026-01-22 17:57:39 +0000

    Deploying to gh-pages from @ pragnyathri-design/pragnayathri-design.github.io@90fbe8bd6d340ab27cf56a8a4d207012929757bf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-volunteering",
           title: "Volunteering",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/Volunteering/";
