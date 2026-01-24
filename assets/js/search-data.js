f9fa2bff0548c976897d4c7c57080a9e77ad1d5d
pragnyathri-design <pragnyathri@gmail.com>
2026-01-24 05:16:05 +0000

    Deploying to gh-pages from @ pragnyathri-design/pragnayathri-design.github.io@94b21b1573355d21503532a4e2086aefc2269198 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-volunteering",
+  },{id: "nav-projects",
+          title: "Projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-volunteering",
           title: "Volunteering",
           description: "",
           section: "Navigation",
